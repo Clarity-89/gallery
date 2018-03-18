bc8d3aac70fc31a04dc9133cd59512d9b2f4cf66
Oleksandr Khomenko <homes89@ukr.net>
2018-03-18 19:56:05 +0200

    Use S3 storage

--- src/gallery/settings/production.py
+++ src/gallery/settings/production.py
@@ -106,3 +106,12 @@ LOGGING['handlers'].update({
 MIDDLEWARE += [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+# AWS Storage
+AWS_STORAGE_BUCKET_NAME = 'gallery-viewer'
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
